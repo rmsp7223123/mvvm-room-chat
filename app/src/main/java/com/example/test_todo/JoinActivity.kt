1a32cc11cb37a8b38539d1d4d752d3d28eb64c13
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-09-27 14:19:08 +0900

    join 끝

--- app/src/main/java/com/example/test_todo/JoinActivity.kt
+++ app/src/main/java/com/example/test_todo/JoinActivity.kt
@@ -1,21 +1,60 @@
 package com.example.test_todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.test_todo.databinding.ActivityJoinBinding
+import com.example.test_todo.model.User
+import com.example.test_todo.repository.UserRepository
+import com.example.test_todo.viewmodel.UserViewModel
 
 
 class JoinActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityJoinBinding;
 
+    private lateinit var userViewModel: UserViewModel;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         binding = ActivityJoinBinding.inflate(layoutInflater);
         setContentView(binding.root);
-
+        userViewModel =ViewModelProvider(this)[UserViewModel::class.java];
         binding.btnCreate.setOnClickListener {
-
+            userViewModel.readAllData.observe(this) { users ->
+                if (users == null || users.none { it.user_id == binding.edtId.text.toString() }) {
+                    insertDataToDatabase(
+                        binding.edtId.text.toString(),
+                        binding.edtPw.text.toString(),
+                        binding.edtNickname.text.toString()
+                    );
+                    Toast.makeText(this, "가입완료", Toast.LENGTH_LONG).show();
+                    startActivity(Intent(this, MainActivity::class.java));
+                } else {
+                    binding.edtId.text = null;
+                    Toast.makeText(this, "중복된 id입니다.", Toast.LENGTH_LONG).show();
+                }
+            }
         };
+    }
+
+    private fun insertDataToDatabase(user_id: String, user_pw: String, user_nickname: String) {
+
+        if(inputCheck(user_id,user_pw,user_nickname)){
+            val user = User(user_id, user_pw, user_nickname);
+            userViewModel.addUser(user);
+            Toast.makeText(this,"추가완료", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "추가실패", Toast.LENGTH_LONG).show();
+        }
+    }
+    
+    private fun inputCheck(user_id:String, user_pw:String, user_nickname: String):Boolean{
+        return !(TextUtils.isEmpty(user_id)&& TextUtils.isEmpty(user_pw)&& TextUtils.isEmpty(user_nickname));
     }
 }
--- app/src/main/java/com/example/test_todo/data/UserDao.kt
+++ app/src/main/java/com/example/test_todo/data/UserDao.kt
@@ -2,13 +2,16 @@ package com.example.test_todo.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.test_todo.model.User
 
 @Dao
 interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addUser(user: User);
 
-    @Query("SELECT * FROM user ORDER BY id ASC")
+    @Query("SELECT * FROM user")
     fun readAllData(): LiveData<List<User>>;
 }
