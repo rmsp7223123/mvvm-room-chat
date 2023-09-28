b54e7a36feb89c723b2e6017ba2ca60b353cd80e
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-09-28 13:53:02 +0900

    홈어댑터 추가

---
+++ app/src/main/java/com/example/test_todo/HomeAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.test_todo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.example.test_todo.databinding.ItemRecvHomeBinding
+import com.example.test_todo.model.User
+
+class HomeAdapter(private val userList: List<User>) : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+
+    inner class ViewHolder(var binding : ItemRecvHomeBinding) : RecyclerView.ViewHolder(
+        binding.root
+    );
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemRecvHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false));
+    };
+
+    override fun getItemCount(): Int {
+        return userList.size;
+    };
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = userList[position];
+        holder.binding.tvId.text = user.user_id;
+        holder.binding.tvName.text = user.user_nickname;
+    };
+};
